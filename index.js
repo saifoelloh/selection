5b24b948e1c802881df2e48d8cf3d2ec03f2126c
saifoelloh <saifoelloh@gmail.com>
2019-07-19 00:51:16 +0700

    solving third problem

--- index.js
+++ index.js
@@ -46,3 +46,31 @@ function second(limit) {
 
 hasil = second(100);
 console.log(`Second result\t: ${hasil}`);
+
+
+/*
+ *  3. Summation of primes
+ */
+function tr_primary(value) {
+    for (let i=2;i<10;i++) {
+        if (value%i==0&&value!==2) {
+            return false;
+        }
+
+    }
+
+    return value===1 ? false : value;
+}
+
+function third(limit) {
+    let hasil = 0;
+    let val = 0;
+    for (let i=1;i<limit;i++) {
+        hasil += tr_primary(i); 
+    }
+
+    return hasil;
+}
+
+hasil = third(2000000);
+console.log(`Third result\t: ${hasil}`);
