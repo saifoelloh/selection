c2580c17574c80de3823df9efd77c58482248e3f
saifoelloh <saifoelloh@gmail.com>
2019-07-18 23:49:40 +0700

    solvinf second problem

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ function first(limit=1000) {
 
     return hasil;
 }
+
+let hasil = first();
+console.log(`First result\t: ${hasil}`);
+
+
+/*
+ * 2. Sum square difference
+ */
+function sec_natural(limit) {
+    let result = 0;
+    for (let i=1;i<=limit;i++) {
+        result += Math.pow(i,2);
+    }
+
+    return result;
+}
+
+function sec_sum(limit) {
+    let temp=0;
+    for (let i=1;i<=limit;i++) {
+        temp += i;
+    }
+
+    return Math.pow(temp, 2);
+}
+
+function second(limit) {
+    const res1 = sec_natural(limit);
+    const res2 = sec_sum(limit);
+
+    return res2 - res1;
+}
+
+hasil = second(100);
+console.log(`Second result\t: ${hasil}`);
